b3417b415ab3908935ea778216a8af5d37914323
Alberto Vallejo <javr1088@gmail.com>
2012-07-16 23:24:55 -0500

    Firs commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kiniela::Application.config.secret_token = '4f26753f6364945e72567f441118cb2c048a814c6607f4c0cc245373f7e118f1155cba2b43ff2bd0bcb74f13ed7346ffbfa5adfb18f5b5591c340517d8862906'
