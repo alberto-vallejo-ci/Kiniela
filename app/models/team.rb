b3962ee0c8234875aebfcea1892236cbc4fb2312
Alberto Vallejo <javr1088@gmail.com>
2012-10-10 22:54:19 -0500

    Add name validation for teams

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,6 +1,10 @@
 class Team < ActiveRecord::Base
+    include ActiveModel::Validations
+
     has_many :gamings
     has_many :games, through: :gamings
+
+    validates_presence_of :name
   
     attr_accessible :name
 end
